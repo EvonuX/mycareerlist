6f0db3d5e25de03437686b13b207560f30dfac78
Djordje Stevanovic <djordje42@gmail.com>
2022-10-17 20:03:34 +0200

    updates

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -3,5 +3,11 @@ import NextAuth from 'next-auth'
 declare module 'next-auth' {
   interface Session {
     userId: string
+    userRole: string
+  }
+
+  interface User {
+    userId: string
+    role: string
   }
 }
